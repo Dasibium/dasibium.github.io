44d016c72835e4499c866c2fceb88c75d7c4f780
Dasibium <108212392+Dasibium@users.noreply.github.com>
2022-08-22 16:57:52 +0100

    22081655
    22081655

--- js/script6.js
+++ js/script6.js
@@ -1,12 +1,3 @@
-const isMobile = () => /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-
-if (isMobile) {
-    mobile();
-  } else {
-    ordinateur();
-  }
-
-
 function mobile() {
     var audio1 = new Audio('https://assets.mixkit.co/sfx/preview/mixkit-sci-fi-interface-zoom-890.mp3');
     audio1.loop = false;
@@ -36,7 +27,7 @@ function dasibium() {
 }
 
 function projets() {
-  document.querySelector('input[type="checkbox2"]').checked = !document.querySelector('input[type="checkbox2"]').checked;
+  document.querySelector('input[type="checkbox"]').checked = !document.querySelector('input[type="checkbox"]').checked;
   let sound1 = "https://assets.mixkit.co/sfx/preview/mixkit-sci-fi-interface-zoom-890.mp3";
   let sound2 = "https://assets.mixkit.co/sfx/preview/mixkit-game-quick-warning-notification-268.mp3";
   let sound3 = "https://assets.mixkit.co/sfx/preview/mixkit-tech-click-1140.mp3";
@@ -44,7 +35,38 @@ function projets() {
   let sound5 = "https://assets.mixkit.co/sfx/preview/mixkit-fast-sci-fi-bleep-903.mp3";
   let sound6 = "https://assets.mixkit.co/sfx/preview/mixkit-high-tech-bleep-2521.mp3";
   let sound7 = "https://assets.mixkit.co/sfx/preview/mixkit-high-tech-bleep-confirmation-2520.mp3";
-  let audio = new Audio(document.querySelector('input[type="checkbox2"]').checked ? sound6: sound6);
+  let audio = new Audio(document.querySelector('input[type="checkbox"]').checked ? sound6: sound6);
+  /*document.getElementById('projets').style.animation="hud-3d 4s linear infinite";*/
+  document.getElementById("middleCircle2").animate(
+  [
+    { transform: 'rotate(-120deg)' }
+  ], {
+    duration: 500,
+	easing: 'ease-in-out',
+	fill: 'forwards',
+    iterations: 1
+  }
+);
+  document.getElementById("innerCircle2").animate(
+  [
+    { transform: 'rotate(-150deg)' }
+  ], {
+    duration: 500,
+	easing: 'ease-in-out',
+	fill: 'forwards',
+    iterations: 1
+  }
+);
+  document.getElementById("outerCircle2").animate(
+  [
+    { transform: 'rotate(360deg)' }
+  ], {
+    duration: 500,
+	easing: 'ease-in-out',
+	fill: 'forwards',
+    iterations: 1
+  }
+);
   let das = document.querySelector('input[type="checkbox"]');
   if (das.checked) {
     das.checked = false;
@@ -58,7 +80,7 @@ function projets() {
 }
 
 function boutique() {
-  document.querySelector('input[type="checkbox3"]').checked = !document.querySelector('input[type="checkbox3"]').checked;
+  document.querySelector('input[type="checkbox"]').checked = !document.querySelector('input[type="checkbox"]').checked;
   let sound1 = "https://assets.mixkit.co/sfx/preview/mixkit-sci-fi-interface-zoom-890.mp3";
   let sound2 = "https://assets.mixkit.co/sfx/preview/mixkit-game-quick-warning-notification-268.mp3";
   let sound3 = "https://assets.mixkit.co/sfx/preview/mixkit-tech-click-1140.mp3";
@@ -66,7 +88,37 @@ function boutique() {
   let sound5 = "https://assets.mixkit.co/sfx/preview/mixkit-fast-sci-fi-bleep-903.mp3";
   let sound6 = "https://assets.mixkit.co/sfx/preview/mixkit-high-tech-bleep-2521.mp3";
   let sound7 = "https://assets.mixkit.co/sfx/preview/mixkit-high-tech-bleep-confirmation-2520.mp3";
-  let audio = new Audio(document.querySelector('input[type="checkbox3"]').checked ? sound6: sound6);
+  let audio = new Audio(document.querySelector('input[type="checkbox"]').checked ? sound6: sound6);
+  document.getElementById("middleCircle3").animate(
+  [
+    { transform: 'rotate(-120deg)' }
+  ], {
+    duration: 500,
+	easing: 'ease-in-out',
+	fill: 'forwards',
+    iterations: 1
+  }
+);
+  document.getElementById("innerCircle3").animate(
+  [
+    { transform: 'rotate(-150deg)' }
+  ], {
+    duration: 500,
+	easing: 'ease-in-out',
+	fill: 'forwards',
+    iterations: 1
+  }
+);
+  document.getElementById("outerCircle3").animate(
+  [
+    { transform: 'rotate(360deg)' }
+  ], {
+    duration: 500,
+	easing: 'ease-in-out',
+	fill: 'forwards',
+    iterations: 1
+  }
+);
   let das = document.querySelector('input[type="checkbox"]');
   if (das.checked) {
     das.checked = false;
